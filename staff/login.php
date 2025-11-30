3f143f0547e8b117c840976182f715542721bb5d
dan-171 <danh.cs0706@gmail.com>
2025-12-01 01:04:36 +0800

    staff

---
+++ staff/login.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  include '../config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Staff Login</title>
+</head>
+</html>

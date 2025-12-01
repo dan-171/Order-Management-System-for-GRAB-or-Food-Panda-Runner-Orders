10dfe6dba97a6a3fabb0088ced23386811511f28
dan-171 <danh.cs0706@gmail.com>
2025-12-01 22:12:43 +0800

    login frontend

--- admin/login.php
+++ admin/login.php
@@ -8,18 +8,17 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Staff Login</title>
+  <title>Admin Login</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../sralogin.css">
-</head>
 <body>
   <div id="banner-top">
       <img src="../images/banner.webp" alt="banner img"/>
   </div>
   <div id="login-box">
-    <h2>Staff Login</h2>
+    <h2>Admin Login</h2>
     <form method="post">
-        <input id="id-input" type="text" name="id" placeholder="Staff ID" required>
+        <input id="id-input" type="text" name="id" placeholder="Admin ID" required>
         <input id="pw-input" type="password" name="password" placeholder="Password" required>
         <button type="submit">Login</button>
     </form>
--- runner/login.php
+++ runner/login.php
@@ -8,22 +8,25 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Staff Login</title>
+  <title>Runner Login</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../sralogin.css">
-</head>
 <body>
   <div id="banner-top">
       <img src="../images/banner.webp" alt="banner img"/>
   </div>
   <div id="login-box">
-    <h2>Staff Login</h2>
+    <h2>Runner Login</h2>
     <form method="post">
-        <input id="id-input" type="text" name="id" placeholder="Staff ID" required>
+        <input id="id-input" type="text" name="id" placeholder="Runner ID" required>
         <input id="pw-input" type="password" name="password" placeholder="Password" required>
         <button type="submit">Login</button>
     </form>
     <p>Forgot ID or Password</p>
+  </div>
+  <div id="delivery-platforms">
+    <img src="../images/grab-logo.webp" alt="grab"/>
+    <img src="../images/foodpanda-logo.webp" alt="grab"/>
   </div>
 </body>
 </html>

13375f081b096360e40402968a7cfd40c75e6f5a
dan-171 <danh.cs0706@gmail.com>
2025-12-09 02:16:53 +0800

    validation

--- sra/sraLogin.js
+++ sra/sraLogin.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
   const loginForm = document.getElementById("login");
   const errMsg = document.getElementById("err-msg");
   loginForm.addEventListener("submit", function formValidation(e){
-    if(loginForm.id.value.trim() == ""){
+    if(loginForm.id.value.trim() === ""){
       e.preventDefault();
       loginForm.id.focus();
       errMsg.textContent = "ID cannot be left blank";
       return;
     }
-    else if (loginForm.pw.value.trim() == ""){
+    else if (loginForm.pw.value.trim() === ""){
       e.preventDefault();
       loginForm.pw.focus();
       errMsg.textContent = "Password cannot be left blank";

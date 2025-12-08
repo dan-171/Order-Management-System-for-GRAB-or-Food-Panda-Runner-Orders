c1b0298890727104920edebd5bdffbd128feb2b6
dan-171 <danh.cs0706@gmail.com>
2025-12-08 15:03:23 +0800

    form submission err msg

---
+++ sra/sraLogin.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const loginForm = document.getElementById("login");
+  const errMsg = document.getElementById("err-msg");
+  loginForm.addEventListener("submit", function formValidation(e){
+    if(loginForm.id.value.trim() == ""){
+      e.preventDefault();
+      loginForm.id.focus();
+      errMsg.textContent = "ID cannot be left blank";
+      return;
+    }
+    else if (loginForm.pw.value.trim() == ""){
+      e.preventDefault();
+      loginForm.pw.focus();
+      errMsg.textContent = "Password cannot be left blank";
+      return;
+    }
+  });  
+});
